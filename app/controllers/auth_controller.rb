dab46ca1483ad09556d640b53e706abfbad19576
aizwellenstan <aizwellenstan@gmail.com>
2019-11-04 00:32:00 +0900

    add login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,11 @@ class AuthController < ApplicationController
         is_authenticated = user.authenticate(params[:password])
 
         if is_authenticated
-            render json: user
+            payload =  { user_id: user.id }
+
+            token = JWT.encode payload, 'wtf', 'HS256'
+
+            render json: { token: token }
         else
             render json: { error: "Wrong username and/or password." }
         end
